199025d790db53fc613d401648b5d2f2e89072cf
Hồ Ngọc Đông Sinh <71634978+dongsinhho@users.noreply.github.com>
2021-10-29 00:28:08 +0700

    fix

--- marketFinance/coinMarket/consumer.py
+++ marketFinance/coinMarket/consumer.py
@@ -7,7 +7,7 @@ class WSConsummer(AsyncJsonWebsocketConsumer):
         await self.accept()
     
     async def disconnect(self,code):
-        await self.channel_layer.group_add('Coin', self.channel_name)       
+        await self.channel_layer.group_discard('Coin', self.channel_name)       
 
     async def send_new_data(self, event):
         data = event['text']
