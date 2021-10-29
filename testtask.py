48d3fe9656fbce001cba26a404ec8ac17aef1ba1
sinh hồ <sinhvua@gmail.com>
2021-10-29 09:55:45 +0700

    ádsada

--- testtask.py
+++ testtask.py
@@ -3,19 +3,25 @@ import ccxt
 #from celery import shared_task
 #from channels.layers import get_channel_layer
 #from asgiref.sync import async_to_sync
+#'DOGE/USDT/' 
 
-
+list_unit = ['BTC/USDT','ETH/USDT', 'ADA/USDT', 'BNB/USDT', 'XRP/USDT','SOL/USDT', 'XRP/USDT','DOT/USDT','USDC/USDT','SHIB/USDT','LUNA/USDT','UNI/USDT','AVAX/USDT','LINK/USDT',
+'LTC/USDT','BUSD/USDT','BCH/USDT','ALGO/USDT','MATIC/USDT','XLM/USDT','VET/USDT','ATOM/USDT','ICP/USDT','ICP/USDT','AXS/USDT', 'FTT/USDT','FIL/USDT','FTM/USDT','TRX/USDT','ETC/USDT']
 
 #def get_coins_data():
-unit_of_currency = 'USDT'
+# unit_of_currency = 'USDT'
 exchange = ccxt.binance()
-markets = exchange.load_markets()
-for market in markets:
-    if(unit_of_currency) in market:
-        data = exchange.fetchOHLCV(symbol=market,timeframe='1m',since=None, limit=1)
-        for candles in data:
-            #obj =  Coin.objects.create(typeCoin=market, value=candles[4])
-            print(market)
+# markets = exchange.load_markets()
+count = 0
+for market in list_unit:
+    # if(unit_of_currency) in market:
+    data = exchange.fetchOHLCV(symbol=market,timeframe='1m',since=None, limit=1)
+    for candles in data:
+        #obj =  Coin.objects.create(typeCoin=market, value=candles[4])
+        count = count + 1
+        print(market)
+print(count)
+            
 
 
 
